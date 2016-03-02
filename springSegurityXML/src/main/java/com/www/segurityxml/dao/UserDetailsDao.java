a1014b067f2e84d822545d2f9626d6abf68ea276
carloncho45 <daleytd@hotmail.com>
2016-03-02 12:18:30 -0500

    comit
    segurity XML 02/03/2016

---
+++ springSegurityXML/src/main/java/com/www/segurityxml/dao/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.www.segurityxml.dao;
+
+import com.www.segurityxml.model.UserAttempts;
+
+public interface UserDetailsDao {
+
+	void updateFailAttempts(String username);
+
+	void resetFailAttempts(String username);
+	
+	UserAttempts getUserAttempts(String username);
+
+}
