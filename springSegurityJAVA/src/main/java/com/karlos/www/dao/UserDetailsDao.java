283115e1e25492cf6e209298179b13b4b65c21d8
carloncho45 <daleytd@hotmail.com>
2016-03-02 11:38:47 -0500

    primer comit
    02/03/2016

---
+++ springSegurityJAVA/src/main/java/com/karlos/www/dao/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.karlos.www.dao;
+
+import com.karlos.www.model.UserAttempts;
+
+public interface UserDetailsDao {
+
+	void updateFailAttempts(String username);
+
+	void resetFailAttempts(String username);
+	
+	UserAttempts getUserAttempts(String username);
+
+}
